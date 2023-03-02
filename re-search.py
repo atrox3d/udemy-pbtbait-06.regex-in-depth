e3bdeb39f98519038bbc7ff4adb9384c69c78669
atrox3d <atrox3d@gmail.com>
2023-03-02 07:16:12 +0100

    23. Intro to RegEx in Python

---
+++ re-search.py
@@ -0,0 +1,21 @@
+import re
+
+text = "The match in Germany"
+found = re.search('^the.*germany', text, re.IGNORECASE)
+
+if found:
+    print("yes, we have a match")
+    print(f'{found=}')
+    print(f'{found.group()=}')
+    print(f'{found.start()=}')
+    print(f'{found.end()=}')
+    print(f'{found.groupdict()=}')
+    print(f'{found.pos=}')
+    print(f'{found.endpos=}')
+    print(f'{found.re=}')
+    print(f'{found.lastgroup=}')
+    print(f'{found.lastindex=}')
+    print(f'{found.lastindex=}')
+    print(f'{found.string=}')
+else:
+    print('no match')
