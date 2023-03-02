51cefb5b72abfdd6590d08fee88c753de0c2aee5
atrox3d <atrox3d@gmail.com>
2023-03-02 09:07:38 +0100

    better output

--- re-search-whitespace.py
+++ re-search-whitespace.py
@@ -7,13 +7,14 @@ match = re.search(r'\s', text)
 
 if match:
     print("yes, we have a match")
+    print(f'{match.string=}')
+    print(f'{match.re=}')
     print(f'{type(match)=}')
     print(f'{match=}')
+    print(f'{match.span()=}')
     print(f'{match.start()=}')
     print(f'{match.end()=}')
     print(f'{match.pos=}')
     print(f'{match.endpos=}')
-    print(f'{match.re=}')
-    print(f'{match.string=}')
 else:
     print('no match')
--- re-search.py
+++ re-search.py
@@ -5,18 +5,19 @@ match = re.search('^the.*germany', text, re.IGNORECASE)     # returns a re.Match
 
 if match:
     print("yes, we have a match")
+    print(f'{match.string=}')
+    print(f'{match.re=}')
     print(f'{type(match)=}')
     print(f'{match=}')
     print(f'{match.group()=}')
+    print(f'{match.span()=}')
     print(f'{match.start()=}')
     print(f'{match.end()=}')
     print(f'{match.groupdict()=}')
     print(f'{match.pos=}')
     print(f'{match.endpos=}')
-    print(f'{match.re=}')
     print(f'{match.lastgroup=}')
     print(f'{match.lastindex=}')
     print(f'{match.lastindex=}')
-    print(f'{match.string=}')
 else:
     print('no match')
