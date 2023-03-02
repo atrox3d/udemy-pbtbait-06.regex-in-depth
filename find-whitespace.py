a8f302a3249c7acb7c849ec3165955d2b7cdcf99
atrox3d <atrox3d@gmail.com>
2023-03-02 07:55:16 +0100

    26. Search for the first white-space character in the string

--- find-whitespace.py
+++ find-whitespace.py
@@ -1,22 +1,19 @@
 import re
 
-text = "The match in Germany"
-match = re.search('^the.*germany', text, re.IGNORECASE)     # returns a re.Match object
+text = 'The rain in Germany'
+
+# find the first space in the string, returns re.Match
+match = re.search(r'\s', text)
 
 if match:
     print("yes, we have a match")
     print(f'{type(match)=}')
     print(f'{match=}')
-    print(f'{match.group()=}')
     print(f'{match.start()=}')
     print(f'{match.end()=}')
-    print(f'{match.groupdict()=}')
     print(f'{match.pos=}')
     print(f'{match.endpos=}')
     print(f'{match.re=}')
-    print(f'{match.lastgroup=}')
-    print(f'{match.lastindex=}')
-    print(f'{match.lastindex=}')
     print(f'{match.string=}')
 else:
     print('no match')
